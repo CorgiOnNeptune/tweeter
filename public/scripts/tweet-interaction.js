7c4121ef5e92330251c440709ee10615833f7c7c
Justin <justinjdbennett@gmail.com>
2022-12-07 04:32:20 +0000

    Change hover effects

--- public/scripts/tweet-interaction.js
+++ public/scripts/tweet-interaction.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-  $('.tweet').hover(function() {
-    $(this).addClass('box-shadow');
-  }, function() {
-    $(this).removeClass('box-shadow');
-  });
-
   $('.tweet-interaction-icons').children().each(function() {
     $(this).hover(function() {
       $(this).addClass('fa-solid');
