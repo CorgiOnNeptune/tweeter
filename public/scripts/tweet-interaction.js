22d5d3adfe121f2788bcbdaf02aadb3eb00ff229
Justin <justinjdbennett@gmail.com>
2022-12-07 04:19:09 +0000

    Add hover event listeners for tweets and icons

--- public/scripts/tweet-interaction.js
+++ public/scripts/tweet-interaction.js
@@ -1,1 +1,15 @@
-$(document).ready
+$(document).ready(function() {
+  $('.tweet').hover(function() {
+    $(this).addClass('box-shadow');
+  }, function() {
+    $(this).removeClass('box-shadow');
+  });
+
+  $('.tweet-interaction-icons').children().each(function() {
+    $(this).hover(function() {
+      $(this).addClass('fa-solid');
+    }, function() {
+      $(this).removeClass('fa-solid');
+    })
+  });
+});
