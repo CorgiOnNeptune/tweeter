71bb38d1887d5aeed49327b5da442949e5b3d893
Justin <justinjdbennett@gmail.com>
2022-12-07 21:31:58 +0000

    Some client-side js function clean-up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,6 +8,47 @@ $(document).ready(function() {
   renderTweets(tempData);
 });
 
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+
+    $('#tweets-container').append($tweet);
+  }
+};
+
+const createTweetElement = function (tweet) {
+  const $tweet = $(`<article>`, { class: 'tweet' });
+
+  // Create 'header' elements of the tweet card
+  const $header = $(`<header>`);
+  const $tweetInfo = $(`<div class="tweet-info"><p>${tweet.user.name}</p></div>`);
+  const $tweetAvatar = $('<img>', {
+    class: "tweet-avatar",
+    src: tweet.user.avatars
+  });
+  const $tweetHandle = $(`<div class="tweet-handle"><p>${tweet.user.handle}</p></div>`)
+
+  const $body = $(`<div class="tweet-body"><p>${tweet.content.text}</p></div>`)
+
+  // Create 'footer' elements of the tweet card
+  const $footer = $(`<footer>`);
+  const $timestamp = $(`<p>${tweet.createdAt}</p>`);
+  const $icons = $(`<div class="tweet-interaction-icons">
+  <i class="fa-regular fa-flag"></i>
+  <i class="fa-regular fa-share-from-square"></i>
+  <i class="fa-regular fa-heart"></i>
+  </div>`);
+
+  $tweetInfo.prepend($tweetAvatar);
+  $header.append($tweetInfo, $tweetHandle);
+  $footer.append($timestamp, $icons);
+  $tweet.append($header, $body, $footer);
+
+  return $tweet;
+};
+
+
+// Temporary test data. TODO - Get from server.
 const tempData = [
   {
     "user": {
@@ -33,45 +74,3 @@ const tempData = [
     "createdAt": 1461113959088
   }
 ];
-
-const createTweetElement = function (tweet) {
-  const $tweet = $('<article>', { class: 'tweet' });
-  const $header = $(`<header>`);
-
-  const $tweetInfo = $(`<div class="tweet-info"><p>${tweet.user.name}</p></div>`);
-
-  const $tweetAvatar = $('<img>', {
-    class: "tweet-avatar",
-    src: tweet.user.avatars
-  });
-
-  const $tweetTag = $(`<div class="tweet-tag"><p>${tweet.user.handle}</p></div>`)
-
-  const $body = $(`<div class="tweet-body"><p>${tweet.content.text}</p></div>`)
-
-  const $footer = $(`<footer>`);
-
-  const $timestamp = $(`<p>${tweet.createdAt}</p>`);
-
-  const $icons = $(`<div class="tweet-interaction-icons">
-  <i class="fa-regular fa-flag"></i>
-  <i class="fa-regular fa-share-from-square"></i>
-  <i class="fa-regular fa-heart"></i>
-  </div>`);
-
-  $tweetInfo.prepend($tweetAvatar);
-  $header.append($tweetInfo, $tweetTag);
-  $footer.append($timestamp, $icons);
-  $tweet.append($header, $body, $footer);
-
-  return $tweet;
-};
-
-const renderTweets = function (tweets) {
-  let $tweet;
-  for (const tweet of tweets) {
-    $tweet = createTweetElement(tweet);
-
-    $('#tweets-container').append($tweet);
-  }
-};
