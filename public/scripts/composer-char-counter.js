307eb08f4d8f9ebd87b9276e11132531d8442690
Justin <justinjdbennett@gmail.com>
2022-12-07 18:13:11 +0000

    Comment and variable name cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#new-tweet-text').on('input', function() {   
-    const counter = $('.counter');
+    const $counter = $('.counter');
     const tweetLength = $(this).val().length;
     const counterLength = 140 - tweetLength;
 
-    counter.toggleClass('red-text', counterLength < 0);
-    counter.val(counterLength);
+    $counter.toggleClass('red-text', counterLength < 0);
+    $counter.val(counterLength);
   })
 });
