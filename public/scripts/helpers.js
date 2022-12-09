06bc20b161fcd95c176c9545bd7f5763a08d9965
Justin <justinjdbennett@gmail.com>
2022-12-09 09:25:24 +0000

    Move more helpers over

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,3 +1,48 @@
+// Render all the tweet elements within the servers data.
+const renderTweets = function (tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+
+    $('#tweets-container').prepend($tweet);
+  }
+};
+
+const createTweetElement = function(tweet) {
+  const timePosted = timeago.format(tweet.createdAt);
+  const $tweet = $(`<article>`, { class: 'tweet' });
+
+  // Create 'header' elements of the tweet card
+  const $header = $(`<header>`);
+  const $tweetInfo = $(`<div class="tweet-info">`);
+  $(`<p>`).text(tweet.user.name).appendTo($tweetInfo);
+  const $tweetAvatar = $('<img>', {
+    class: "tweet-avatar",
+    src: tweet.user.avatars
+  });
+  const $tweetHandle = $(`<div class="tweet-handle"><p>${tweet.user.handle}</p></div>`);
+
+  const $body = $(`<div class="tweet-body">`);
+  $(`<p>`).text(tweet.content.text).appendTo($body);
+
+  // Create 'footer' elements of the tweet card
+  const $footer = $(`<footer>`);
+  const $timestamp = $(`<p>${timePosted}</p>`);
+  const $icons = $(
+    `<div class="tweet-interaction-icons">
+    <i class="fa-regular fa-flag"></i>
+    <i class="fa-regular fa-share-from-square"></i>
+    <i class="fa-regular fa-heart"></i>
+    </div>`
+  );
+
+  $tweetInfo.prepend($tweetAvatar);
+  $header.append($tweetInfo, $tweetHandle);
+  $footer.append($timestamp, $icons);
+  $tweet.append($header, $body, $footer);
+
+  return $tweet;
+};
+
 // Function targets each icon child in the tweet article footers to fill the icons in with solid color.
 const solidIcons = () => {
   $('.tweet-interaction-icons').children().each(function() {
